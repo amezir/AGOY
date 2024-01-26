947ac770cf9f5204be105c0945f59391741c3e9b
amezir <amezirmessaoud.pro@gmail.com>
2024-01-26 16:58:28 +0100

    add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Charger la scène de jeu
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // Quitter l'application
+        Application.Quit();
+    }
+}
